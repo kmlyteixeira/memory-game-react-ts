59c4bc4d4b8cc4d91ccf2be1a88a550789446ef3
kmly <kemilyteixeira047@gmail.com>
2022-09-09 21:52:58 -0300

    :wrench: style: alinhamento do conatiner e do header

--- src/styled/index.tsx
+++ src/styled/index.tsx
@@ -1,15 +1,29 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+export const Font = css `
+    @font-face {
+        font-weight: normal;
+        font-style: normal;
+        src: url('get-schwifty.ttf') format('truetype');
+    }
+`;
 
 export const TitlePage = styled.h1 `
     color: red;
     font-size: 50px;
+    font-family: ${Font};
 `;
 
 export const Container = styled.div `
+    position: fixed;
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-template-rows: repeat(5, 1fr);
+    left: 30%;
 `;
 
 export const List = styled.ul `
-    display: flex;
+    list-style: none;
 `;
 
 export const Item = styled.li `
@@ -18,4 +32,9 @@ export const Item = styled.li `
 
 export const Image = styled.img `
     width: 100px;
+`;
+
+export const Header = styled.div `
+    display: flex;
+    justify-content: center;
 `;
